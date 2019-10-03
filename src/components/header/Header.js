48bae7283ab225cadc935c257961ef872b2dda9a
Sulaiman Alhabib <suldev8@gmail.com>
2019-10-03 21:09:42 +0300

    Remove Contact and About nav links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,8 +7,6 @@ const Header = () => {
             <nav>
                 <a href="#home">Home</a>
                 <a href="#projects">Projects</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
             </nav>
         </header>
     )
