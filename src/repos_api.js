11cd5b976f68d70481ef2d6b2361408b3a53f75d
Sulaiman Alhabib <suldev8@gmail.com>
2019-10-07 14:42:13 +0300

    Update projects data

--- src/repos_api.js
+++ src/repos_api.js
@@ -15,15 +15,11 @@ export default [
 },
 {
     id: 177447278,
-    project_url: "https://wwww.google.com"
+    project_url: "https://suldev8.github.io/memory-game/"
 },
 {
     id: 189936565,
     project_url: "https://suldev8.github.io/restaurant-reviews-app/"
-},
-{
-    id: 206377994,
-    project_url: "https://wwww.google.com"
 },
 {
     id: 194093998,
@@ -31,7 +27,6 @@ export default [
 },
 {
     id: 139003699,
-    project_url: "https://wwww.google.com"
 },
 {
     id: 207286192,
